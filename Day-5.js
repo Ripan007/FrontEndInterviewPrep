e37ddf1d3499f0ef5236ac90b62f410acdfb68d3
Ripan Mondal <92440897+Ripan007@users.noreply.github.com>
2022-10-09 23:33:40 +0530

    add

---
+++ Day-5.js
@@ -0,0 +1,28 @@
+// 1. reverse  a given string
+
+// var name = "codewithme";
+// var reverse = name.split("").reverse().join("");
+// console.log(reverse);
+
+// ans : emhtiwedoc;
+
+// 2.Find the sum of all elements/numbers of a given array?
+// without using loop
+// var arr = [1, 2, 6, 7];
+// var sum = arr.reduce((a, i) => {
+//   return a + i;
+// });
+
+// console.log(sum);
+
+// using loop
+
+// var arr = [3, 6, 7, 8];
+// let sum = 0;
+// for (let i in arr) {
+//   sum += arr[i];
+// }
+
+// console.log(sum);
+
+link : https://www.fullstacktutorials.com/interviews/javascript-coding-questions-answers-6.html
