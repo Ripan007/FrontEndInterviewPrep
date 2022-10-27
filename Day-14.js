b63440a30b5e8e7c42852b8e01d159e4f10c24b8
Ripan Mondal <92440897+Ripan007@users.noreply.github.com>
2022-10-27 18:52:29 +0530

    add

--- Day-14.js
+++ Day-14.js
@@ -1,15 +1,17 @@
+// Create an 8 * 8 chessboard UI using html, css and js.Each box size will be 20px * 20px.Write modular code so that it is easier to extend.Modular means reusable and less of repetition
 
-//     first question
-//     create a 8 * 8 chessboard UI using html, css, js.Each box size will be 20px * 20px
-//     Optimisation expected:write modular code so that it is easier to extend.Modular means reusable and less of
-// repetition
+document.write("<table border='1' width='500' height='500'  align='center' style='max-width:100%;max-height:100%;'>")
 
+for (var i = 0; i < 8; i++) {
+    document.write("<tr>");
+    for (var j = 0; j < 8; j++) {
+        if ((i + j) % 2 != 0) {
+            document.write("<td bgcolor='white'</td>")
+        } else {
+            document.write("<td bgcolor='black'> </td>")
+        }
+    }
+    document.write("</tr>")
+}
+document.write("</table>")
 
-//     second question
-//     write a recursive JS function which takes a JSON object and prints all the values present in the object.
-//     input = { a: 1, b: 2 }
-// output = { b: { c: 2 } }
-// output = 2;
-//     your code here which print all the values of the json object
-// const print.json = (jsonObj) => { }
-// Note:Don't use simply JSON.stringify(jsonObj) as it would just return objects in stringified notation . we only want values in json objects 
